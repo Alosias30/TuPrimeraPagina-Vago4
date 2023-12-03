a9bcf1697aea695e862b746300e56db373112b91
Agustin <aleo3007@gmail.com>
2023-12-03 03:45:36 -0300

    "Segundo commit"

--- CoderApp/models.py
+++ CoderApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Comentario(models.Model):
+    nombre = models.CharField(max_length=50)
+    comentario = models.CharField(max_length=300)
+
+class Destino(models.Model):
+    nombre = models.CharField(max_length=50)
+    comentario = models.CharField(max_length=300)
+
+class Restaurant(models.Model):
+    nombre = models.CharField(max_length=50)
+    comentario = models.CharField(max_length=300)
